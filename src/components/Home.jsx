f6e37a015de2c871a5d06d8cc09f26abdafe6875
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-07-20 09:24:07 +0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,14 +3,23 @@ import { Link } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 
 const Home = () => {
-  const user = useContext(UserContext);
+  const [user, setUser] = useContext(UserContext);
 
   return (
     <div>
-      <h1 className="text-center text-3xl my-2">
-        Welcome Home,
-        <b className="text-blue-900">{user}</b>
-      </h1>
+      <div className="flex flex-col items-center justify-center my-3">
+        <h1 className="text-center text-3xl my-2">
+          Welcome Home,
+          <b className="text-blue-900">{user}</b>
+        </h1>
+        <button
+          className="bg-blue-600 py-2 px-3 rounded-lg text-white mx-auto"
+          onClick={() => setUser("Georges")}
+          type="button"
+        >
+          Edit Name
+        </button>
+      </div>
       <div className="flex flex-row gap-3 justify-center items-center font-bold text-blue-500">
         <Link to="/dashboard">Dashboard</Link>
         <Link to="/recommendations">Recommendations</Link>
