6d6cfcc16bea83190d28818b96f07ec1734b7978
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-07-20 09:17:28 +0300

    Update Podcasts.jsx

--- src/components/Podcasts.jsx
+++ src/components/Podcasts.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../context/UserContext";
 
 const Podcasts = () => {
-  return <div>Your Podcasts</div>;
+  const user = useContext(UserContext);
+
+  return (
+    <div>
+      <h1 className="text-center text-2xl">
+        Your Podcasts <b>{user}</b>
+      </h1>
+    </div>
+  );
 };
 
 export default Podcasts;
