0494244a912b923b23997b4850bda199c930cef0
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-07-20 09:09:23 +0300

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../context/UserContext";
 
 const Dashboard = () => {
-  return <div>Your Dashboard</div>;
+  const user = useContext(UserContext);
+
+  return (
+    <div>
+      <h1 className="text-center text-2xl">
+        Hello, <b>{user}</b>
+      </h1>
+      <h2 className="text-center text-xl">Your Dashboard</h2>
+    </div>
+  );
 };
 
 export default Dashboard;
