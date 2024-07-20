c2ada78b9a831aceb67e244bb3afdec4359c04eb
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-07-20 09:17:25 +0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
 
 const Home = () => {
+  const user = useContext(UserContext);
+
   return (
     <div>
-      <h1 className="text-center text-3xl">Home</h1>
-      <div className="flex flex-row gap-3 justify-center items-center font-bold">
+      <h1 className="text-center text-3xl my-2">
+        Welcome Home,
+        <b className="text-blue-900">{user}</b>
+      </h1>
+      <div className="flex flex-row gap-3 justify-center items-center font-bold text-blue-500">
         <Link to="/dashboard">Dashboard</Link>
         <Link to="/recommendations">Recommendations</Link>
         <Link to="/podcasts">Podcasts</Link>
