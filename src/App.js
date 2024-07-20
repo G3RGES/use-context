898a4644b772007608e2eb5c873946565f844fca
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-07-20 08:47:23 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Podcasts from "./components/Podcasts";
+import Dashboard from "./components/Dashboard";
+import Recommendations from "./components/Recommendations";
+
 function App() {
   return (
     <>
       <h1 className="text-3xl text-center py-6 my-2">useContext</h1>
+
+      <Router>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/podcasts" element={<Podcasts />} />
+          <Route path="/recommendation" element={<Recommendations />} />
+        </Routes>
+      </Router>
     </>
   );
 }
