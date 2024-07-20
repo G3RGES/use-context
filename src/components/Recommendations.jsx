d17bcb35e02eab160ba6af107c337cd97119babf
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-07-20 09:17:30 +0300

    Update Recommendations.jsx

--- src/components/Recommendations.jsx
+++ src/components/Recommendations.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../context/UserContext";
 
 const Recommendations = () => {
+  const user = useContext(UserContext);
+
   return (
     <div>
-      <h1 className="">Your Recommendations</h1>
+      <h1 className="text-center text-2xl">
+        Your Recommendations, <b>{user}</b>
+      </h1>
     </div>
   );
 };
